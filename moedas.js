ed67a743cb1ca3c8a38b24cd89f0650f4014a511
Devanir <61068102+De472@users.noreply.github.com>
2020-05-26 21:00:48 -0300

    escolhe entre brl, usd e eur. digite o valor

--- moedas.js
+++ moedas.js
@@ -1,17 +1,22 @@
 
+//vareaveis para serem usadas no arquivo html
+var real_dolar
+var real_euro
+var dolar_real
+var dolar_euro
+var euro_real
+var euro_dolar
+
 //chamando a api com os valores das moedas em forma de função
 $.getJSON("http://data.fixer.io/api/latest?access_key=9d3b2fafe395e806ec62c3010e61ca37", function(dados){
-  //para verificar se deu certo e referência. Console da página
+  //para verificar se deu certo e usar como referência. Console da página
   console.log(dados);
 
-  //placeholder - mostra os valores do euro e dólar
-  document.write("Real: 1.000" + "<br>");
-
-  var dolar = dados.rates.BRL / dados.rates.USD
-  //deixei pra mostrar até o terceiro digito depois da vírgula
-  document.write("Dólar Americano: " + dolar.toString(10).substring(0, 5) + "<br>");
-
-  var euro = dados.rates.BRL
-  document.write("Euro: " + euro.toString(10).substring(0, 5) + "<br>");
+  real_dolar = (dados.rates.BRL / dados.rates.USD)
+  real_euro = (dados.rates.BRL / dados.rates.EUR)
+  dolar_real = (dados.rates.USD / dados.rates.BRL)
+  dolar_euro = (dados.rates.USD / dados.rates.EUR)
+  euro_real = dados.rates.BRL
+  euro_dolar = dados.rates.USD
 
 });
