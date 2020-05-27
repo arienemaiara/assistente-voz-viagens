46761eda3c03abf813b70bd7d26207b2f4be93d1
Tairik Johnny <tairikjohnny@gmail.com>
2020-05-26 21:12:29 -0300

    Alterei a chave da API

--- busca-clima/main.js
+++ busca-clima/main.js
@@ -3,7 +3,7 @@ function getUserPosition() {
     navigator.geolocation.getCurrentPosition((pos) => {
       let lat = pos.coords.latitude;
       let long = pos.coords.longitude;
-      url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=imperial&APPID=95b11822eb429c84c1143a19251b1881`;
+      url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=imperial&APPID=0ed1849b155148f92803761f3cb5b7ce`;
       fetchApi(url);
     });
   }
