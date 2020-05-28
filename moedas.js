cfa54b06a957df57ce28463d8c8abf95cd57245a
Devanir <61068102+De472@users.noreply.github.com>
2020-05-28 09:50:57 -0300

    arrumado - conversao estava invertida

--- moedas.js
+++ moedas.js
@@ -12,10 +12,10 @@ $.getJSON("http://data.fixer.io/api/latest?access_key=9d3b2fafe395e806ec62c3010e
   //para verificar se deu certo e usar como referência. Console da página
   console.log(dados);
 
-  real_dolar = (dados.rates.BRL / dados.rates.USD)
-  real_euro = (dados.rates.BRL / dados.rates.EUR)
-  dolar_real = (dados.rates.USD / dados.rates.BRL)
-  dolar_euro = (dados.rates.USD / dados.rates.EUR)
+  real_dolar = (dados.rates.USD / dados.rates.BRL)
+  real_euro = (dados.rates.EUR / dados.rates.BRL)
+  dolar_real = (dados.rates.BRL / dados.rates.USD)
+  dolar_euro = (dados.rates.EUR / dados.rates.USD)
   euro_real = dados.rates.BRL
   euro_dolar = dados.rates.USD
 
