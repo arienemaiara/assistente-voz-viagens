431405df0829f3b1b59ff065a7485427cc6babdb
Jeferson <jefh.neves@gmail.com>
2020-05-28 12:44:37 -0300

    Update02. Adicionado fix da calculadora e novo layout.

---
+++ assistente-voz-viagens-v0.01/js/iniciarvoz2.js
@@ -0,0 +1,18 @@
+(() => {
+    const startBtn = document.querySelector('#start2');
+    function start() {
+      const recognition = new webkitSpeechRecognition();
+      recognition.interimResults = true;
+      recognition.lang = "pt-BR";
+      recognition.continuous = true;
+      recognition.start();
+      recognition.onresult = function(event) {
+        for (let i = event.resultIndex; i < event.results.length; i++) {
+          if (event.results[i][0].transcript.trim()=== 'calculadora'){
+            window.open('pagina-conversao-moedas.html');
+          }
+          }
+        }
+      };
+    startBtn.addEventListener('click', () => start());
+  })();
