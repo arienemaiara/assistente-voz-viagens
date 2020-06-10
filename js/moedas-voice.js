b02a992510ac8d7e794ae32ac90de554d3dae6ff
Devanir <61068102+De472@users.noreply.github.com>
2020-06-10 16:54:09 -0300

    comentarios atualizados

--- js/moedas-voice.js
+++ js/moedas-voice.js
@@ -14,13 +14,17 @@ recognition.onresult = function(event) {
   for (let i = event.resultIndex; i < event.results.length; i++) {
     var text = (event.results[i][0].transcript.trim())
     //frase: VALOR de MOEDA1 para MOEDA2
+
+    //isso vai garantir que tudo seja letra minúscula
     const texto = text.toLowerCase()
     console.log(texto)
     //identifica onde o valor termina
     var valor_fim = texto.indexOf("d")
     console.log(valor_fim)
+    //nova string sem o valor
     var texto_sem_valor = texto.slice(valor_fim,)
     console.log(texto_sem_valor)
+    //isola o valor
     var valor = texto.slice(0, valor_fim)
     console.log(valor)
     //pega a primeira letra da primeira moeda
@@ -30,11 +34,14 @@ recognition.onresult = function(event) {
     var moeda2 = texto_sem_valor.slice(-6)
     console.log(moeda2)
 
+    //verifica a primeira letra da moeda
     if (moeda1 == "r") {
+      //LAR é unico de dólar
       if (moeda2.includes("lar")) {
-        //as vezes o voice identifica "1" como "um"
+        //as vezes o voice identifica "1" como "um", pra garantir
         if (valor.includes("um")) {
-        alert ("$ " + real_dolar.toFixed(3))
+          //real_dolar é a variável que está no moedas.js, é a conversao
+          alert ("$ " + real_dolar.toFixed(3))
         }
         else {
           var valor_convertido = real_dolar * valor
