a49db8b0767e85f71032b99ddfdecbc37e4ca60b
Devanir <61068102+De472@users.noreply.github.com>
2020-06-11 13:34:10 -0300

    fix e pega valores atualizados

--- js/converter.js
+++ js/converter.js
@@ -12,37 +12,66 @@ window.onload = function() {
 
 //pega o resultado da recognition.start() e compara
 recognition.onresult = function(event) {
-  
+
   for (let i = event.resultIndex; i < event.results.length; i++) {
-      var text = event.results[i][0].transcript.trim();
+      var texto = event.results[i][0].transcript.trim();
+      const text = texto.toLowerCase()
+      console.log(text)
 
     if (text.includes('converter')||text.includes('Converter')) {
         var result = 0;
         var inicio3 = text.indexOf('p')
+        console.log(inicio3)
         var endMoney = inicio3-1;
-        var money = text.slice(13,endMoney);
-        var currency = '';
-        var symbol = '';
+        console.log(endMoney)
+        var money
+        var currency
+        var symbol = ''
         var response = ''
 
-        if(text.indexOf('d') > 0){
-            currency = 'dólar';
-            symbol = '$';
-        } else if(text.indexOf('u') > 0){
-            currency = 'euro';
-            symbol = '€'
+        if (text.includes("r$")) {
+          currency = "R$"
+          money = text.slice(13,endMoney);
+          if(text.indexOf('d') > 0){
+              symbol = '$';
+              result = money*real_dolar;
+
+          } else if(text.indexOf('u') > 0){
+              symbol = '€'
+              result = money*real_euro;
+          }
         }
 
-        if(currency == 'dólar'){
-            result = money*0.2;
-        }else if (currency=='euro'){
-            result = money*0.18;
+        else if (text.includes(" $")) {
+          currency = "$"
+          money = text.slice(11,endMoney);
+          if (text.includes("rea")) {
+            symbol = "R$"
+            result = money*dolar_real;
+          } else if (text.includes("euro")) {
+            symbol = "€"
+            result = money*dolar_euro;
           }
+        }
 
-        response = "R$"+money + " equivale a " + symbol + result.toFixed(2);
+        else if (text.indexOf("u") < inicio3) {
+          currency = "€"
+          money = text.slice(10, (endMoney -5))
+          if (text.includes("real", endMoney)) {
+            symbol = "R$"
+            result = money*euro_real
+          } else if (text.includes("dólar", endMoney)) {
+            symbol = "$"
+            result = money*euro_dolar
+          }
+        }
+
+
+
+        response = currency + money + " equivale a " + symbol + result.toFixed(2);
         document.getElementById('resultconvert').innerHTML = response;
+      }
 
-    }
     else if (event.results[i][0].transcript.trim()=== 'Walker voltar'){
       window.location.href = 'tips.html';
     }
@@ -50,10 +79,8 @@ recognition.onresult = function(event) {
       window.location.href = 'index.html';
     }
 
-    else 
+    else
     document.getElementById('resultconvert').innerHTML = "Não entendi!"
 
   }
 };
-
-
