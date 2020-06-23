9a2a2ff66629a233d683709a89778ecf2104c994
Jonatas Ferreira <jonatas.ferreira7@outlook.com>
2020-06-23 09:41:05 -0300

    add função fechar documentos

--- js/maisdocs.js
+++ js/maisdocs.js
@@ -11,11 +11,19 @@ window.onload = function() {
 
 recognition.onresult = function(event) {
   for (let i = event.resultIndex; i < event.results.length; i++) {
+
+    var janela = window.open(('https://www.anac.gov.br/assuntos/passageiros/documentos-para-embarque'), "janela");
+
     if (event.results[i].isFinal && event.results[i][0].transcript.trim()=== 'documento') {
-      window.open('https://www.anac.gov.br/assuntos/passageiros/documentos-para-embarque');
+      janela;
     }
-    if (event.results[i][0].transcript.trim()=== 'voltar'){
+
+    else if (event.results[i].isFinal && event.results[i][0].transcript.trim()==='fechar') {
+      janela.close();
+    }
+    else if (event.results[i].isFinal && event.results[i][0].transcript.trim()==='voltar'){
       window.location.href = 'homepage.html';
+      janela.close();
     }
   }
 };
