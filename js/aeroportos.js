73b87a5879037d025f93d1658c6b07da9dbc83c9
Devanir <61068102+De472@users.noreply.github.com>
2020-06-27 15:58:31 -0300

    Add files via upload

---
+++ js/aeroportos.js
@@ -0,0 +1,28 @@
+
+var estado
+
+function api_aeroportos(){
+
+	var settings = {
+		"async": true,
+		"crossDomain": true,
+		"url": "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/autosuggest/v1.0/BR/BRL/en-GB/?query=" + estado,
+		"method": "GET",
+		"headers": {
+			"x-rapidapi-host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
+			"x-rapidapi-key": "ddcf87fc70mshcfe365706babc7dp1bfffbjsnefd246949f22"
+		}
+	}
+
+	$.ajax(settings).done(function (response) {
+		console.log(response);
+		var resposta = ""
+
+		for (var i = 0; i < response.Places.length; i++) {
+			var aeroportos = response.Places[i];
+			resposta = resposta + aeroportos.PlaceName + "<br>"
+		}
+
+	document.getElementById("resultado").innerHTML = resposta
+	});
+}
