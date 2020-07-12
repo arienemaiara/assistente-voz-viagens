0b79b99efaa690cac4c97b34050bd6dc9ea88d1c
Devanir <61068102+De472@users.noreply.github.com>
2020-07-12 00:53:05 -0300

    comentario atualizado

--- js/moedas.js
+++ js/moedas.js
@@ -1,5 +1,5 @@
 
-//variáveis para serem usadas no arquivo html
+//variáveis para serem usadas nos outros arquivos
 var real_dolar
 var real_euro
 var dolar_real
@@ -7,7 +7,6 @@ var dolar_euro
 var euro_real
 var euro_dolar
 
-//chamando a api com os valores das moedas em forma de função
 $.getJSON("http://data.fixer.io/api/latest?access_key=9d3b2fafe395e806ec62c3010e61ca37", function(dados){
   //para verificar se deu certo e usar como referência. Console da página
   console.log(dados);
