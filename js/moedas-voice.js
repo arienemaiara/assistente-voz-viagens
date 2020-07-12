ca90fb21fefa392d88e5517b031ac56b510a42a9
Devanir <61068102+De472@users.noreply.github.com>
2020-07-12 00:51:03 -0300

    comentario atualizado

--- js/moedas-voice.js
+++ js/moedas-voice.js
@@ -18,37 +18,47 @@ recognition.onresult = function(event) {
     //isso vai garantir que tudo seja letra minúscula
     const texto = text.toLowerCase()
     console.log(texto)
+    
     //identifica onde o valor termina
     var valor_fim = texto.indexOf("d")
     console.log(valor_fim)
+    
     //nova string sem o valor
     var texto_sem_valor = texto.slice(valor_fim,)
     console.log(texto_sem_valor)
+    
     //isola o valor
     var valor = texto.slice(0, valor_fim)
     console.log(valor)
+    
     //pega a primeira letra da primeira moeda
     var moeda1 = texto[valor_fim + 3]
     console.log(moeda1)
+    
     //variavel que irá identificara segunda moeda depois
     var moeda2 = texto_sem_valor.slice(-6)
     console.log(moeda2)
     var valor_convertido
-    //verifica a primeira letra da moeda
+    
+    //verifica a primeira letra da moeda1
     if (moeda1 == "r") {
+      
       //LAR é unico de dólar
       if (moeda2.includes("lar")) {
+        
         //as vezes o voice identifica "1" como "um", pra garantir
         if (valor.includes("um")) {
+          
           //real_dolar é a variável que está no moedas.js, é a conversao
           document.getElementById("resultado").innerHTML = "$ " + real_dolar.toFixed(2)
         }
         else {
-          //para qualquer número que o comando de voz entender
+          //para qualquer outro número que o comando de voz entender
           valor_convertido = real_dolar * valor
           document.getElementById("resultado").innerHTML = "$ " + valor_convertido.toFixed(2)
         }
       }
+      //EU é exclusivo de euro
       else if (moeda2.includes("eu")) {
         if (valor.includes("um")) {
           document.getElementById("resultado").innerHTML = "€ " + real_euro.toFixed(2)
@@ -64,6 +74,7 @@ recognition.onresult = function(event) {
     }
 
     else if (moeda1 == ("d")) {
+      //RE é exclusivo de real
       if (moeda2.includes("re")) {
         if (valor.includes("um")) {
           document.getElementById("resultado").innerHTML = "R$ " + dolar_real.toFixed(2)
